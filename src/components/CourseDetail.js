6245e7445d6b5b4b35e1d04d146c01257b5e482c
hossainshadat <hossainshadat501@gmail.com>
2022-10-28 16:00:28 +0600

    Clickable pdf download fixed

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -3,7 +3,23 @@ import { useLoaderData } from "react-router-dom";
 
 const CourseDetail = () => {
   const data = useLoaderData();
-  console.log(data.title);
+
+  // Function will execute on click of button
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch("roadmap.pdf").then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "roadmap.pdf";
+        alink.click();
+      });
+    });
+  };
+
   return (
     <div className="flex justify-center py-16">
       <a
@@ -22,7 +38,10 @@ const CourseDetail = () => {
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {data?.details}
           </p>
-          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+          <button
+            onClick={onButtonClick}
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+          >
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Download Pdf
             </span>
